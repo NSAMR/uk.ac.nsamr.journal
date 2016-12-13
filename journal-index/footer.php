2a74cf6e16e64a388f642eecb29027b49ffef0b0
Helen Jackson <helen@deckofpandas.co.uk>
2016-12-13 20:46:06 +0000

    Split out header and footer for reuse

---
+++ journal-index/footer.php
@@ -0,0 +1,94 @@
+        <!--footer start-->
+        <footer class="footer" id="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 margin-btm-30">
+                        <h3>Courses</h3>
+                        <ul class="list-unstyled f-list">
+                            <li><a href="#">Web Designing</a></li>
+                            <li><a href="#">Web Development</a></li>
+                            <li><a href="#">App Development</a></li>
+                            <li><a href="#">Photoshop psd</a></li>
+                            <li><a href="#">Marketing</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 margin-btm-30">
+                        <h3>Quick links</h3>
+                        <ul class="list-unstyled f-list">
+                            <li><a href="#">Apply now</a></li>
+                            <li><a href="#">Events</a></li>
+                            <li><a href="#">News</a></li>
+                            <li><a href="#">Courses</a></li>
+                            <li><a href="#">Terms & Conditions</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 margin-btm-30">
+                        <h3>Recent Post</h3>
+                        <ul class="list-unstyled f-post">
+                            <li class="clearfix">
+                                <a href="#" class="thumb"><img src="static/images/g1.jpg" alt="" width="70" class="img-responsive"></a>
+                                <div class="f-post-content">
+                                    <a href="#">
+                                        Post title goes here
+                                    </a>
+                                    <em>2 days ago</em>
+                                </div>
+                            </li>
+                            <li class="clearfix">
+                                <a href="#" class="thumb"><img src="static/images/g2.jpg" alt="" width="70" class="img-responsive"></a>
+                                <div class="f-post-content">
+                                    <a href="#">
+                                        Post title goes here
+                                    </a>
+                                    <em>2 days ago</em>
+                                </div>
+                            </li>
+                            <li class="clearfix">
+                                <a href="#" class="thumb"><img src="static/images/g3.jpg" alt="" width="70" class="img-responsive"></a>
+                                <div class="f-post-content">
+                                    <a href="#">
+                                        Post title goes here
+                                    </a>
+                                    <em>2 days ago</em>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 margin-btm-30">
+                        <h3>Contact us</h3>
+                        <ul class="list-inline footer-social">
+                            <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        </ul>
+                        <p class="phone"><i class="fa fa-phone"></i> +0034 43244 44</p>
+                        <p>Monday - Friday - 9:Am to 5 Pm</p>
+                        <p>Questions: <a href="#">support@domain.com</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <div class="container">
+                    <span>&copy; Copyright 2016. All Right Reserved. Design_mylife</span>
+                </div>
+            </div>
+        </footer>
+        <!--footer end-->
+
+        <!-- jQuery -->
+        <script src="static/js/jquery.min.js"></script>
+        <script src="static/bootstrap/js/bootstrap.min.js"></script>
+        <script src="static/js/wow.min.js"></script>
+        <script src="static/js/bootstrap-hover-dropdown.min.js"></script>
+        <script src="static/js/jquery.stellar.min.js"></script>
+        <script src="static/flexslider/jquery.flexslider-min.js"></script>
+        <script src="static/owl-carousel/owl.carousel.min.js"></script>
+        <script src="static/js/custom.js"></script>
+
+        <!--revolution slider plugins-->
+        <script src="static/rs-plugin/js/jquery.themepunch.tools.min.js" type="text/javascript"></script>
+        <script src="static/rs-plugin/js/jquery.themepunch.revolution.min.js" type="text/javascript"></script>
+        <script type="text/javascript" src="static/js/revolution-custom.js"></script>
+    </body>
+</html>
