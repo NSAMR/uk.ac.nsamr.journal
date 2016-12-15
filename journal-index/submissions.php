4cfc88f82701eb96599706eac12bcf2acb814707
Helen Jackson <helen@deckofpandas.co.uk>
2016-12-15 11:00:10 +0000

    Author guidlines (RENAME THIS)

---
+++ journal-index/submissions.php
@@ -0,0 +1,70 @@
+<!---STANDARD HEAD INFO AND NAV-->
+<?php include('header.php'); ?>
+
+<!---BODY OF THIS PAGE-->
+
+
+<!--call to action-->
+<div class="pattern-bg-mid-shorter">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8">
+                <div class="left-title-bar">
+                    <h2>Author guidelines</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--end call to action-->
+
+<div class="space40"></div> 
+
+<div class="container">
+            <div class="row">
+                <div class="col-sm-9">
+                    <div class="divide80"></div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="left-title">Online submission</div>
+                            <div>
+                                <p>Authors must <a href="#">log in</a> or <a href=-"#">register</a> with JNSAMR before they can submit articles online.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="divide30"></div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="left-title">Eligibility</div>
+                            <div>
+                                <p>All articles must have a medical student as first author and corresponding author. The medical student must be enrolled at an accredited medical school.</p>
+                                <p>The first author must submit the article using their institutional email address (e.g. @ac.uk, @.edu). The editors retain the right to contact the first author's institution to confirm their enrollment.</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+                <!--SIDEBAR-->
+                <div class="col-sm-3 margin40">
+                    <div class="side-nav-wrapper">
+                        <ul class="list-unstyled side-nav">
+                        <li><a href="#" class="active"><i class="fa fa-cloud"></i> Side Nav</a></li>
+                        <li><a href="#"><i class="fa fa-user"></i> About Us</a></li>
+                        <li><a href="#"><i class="fa fa-dollar"></i> Our Pricing</a></li>
+                        <li><a href="#"><i class="fa fa-bars"></i> Our Services</a></li>
+                        <li><a href="#"><i class="fa fa-users"></i> Our Team</a></li>
+                        <li><a href="#"><i class="fa fa-users"></i> Our Team</a></li>
+                        <li><a href="#"><i class="fa fa-search"></i> 404 Page</a></li>
+                    </ul>
+                    </div>
+                </div><!--SIDEBAR END-->
+            </div>
+        </div><!--side navigation container-->
+
+
+<div class="space40"></div> 
+
+
+<!---PAGE FOOTER AND SCRIPTS-->
+<?php include('footer.php'); ?>
