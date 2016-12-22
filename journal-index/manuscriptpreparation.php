cb05b668f133c7ad0e5588d06df33a2e57eb354a
Helen Jackson <helen@deckofpandas.co.uk>
2016-12-22 05:12:14 +0000

    Manuscript prep section has been split off and accordion-ed

---
+++ journal-index/manuscriptpreparation.php
@@ -0,0 +1,193 @@
+<div class="row">
+    <div class="col-sm-12">
+
+
+
+        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="panel panel-default">
+
+                <div class="panel-heading" role="tab" id="fmc">
+                    <h4 class="panel-title">
+                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse-fmc" aria-expanded="true" aria-controls="fmc">
+                            Flexible manuscipt content
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse-fmc" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="fmc">
+                    <div class="panel-body">
+                        <p>Authors are not required by JNSAMR to include any specific headings or contents.</p>
+                    </div>
+                </div>
+
+
+                <div class="panel-heading" role="tab" id="sg">
+                    <h4 class="panel-title">
+                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse-sg" aria-expanded="false" aria-controls="sg">
+                            Style guidelines
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse-sg" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="sg">
+                    <div class="panel-body">
+                        <p>All manuscripts must adhere to the following style guidelines, otherwise they will be rejected outright. Please see the example submission (HYPERLINK XX) and submission template (HYPERLINK XX). Manuscript contents for each article type can be found under the relevant article in article policies.</p>
+                    </div>
+                </div>
+
+
+                <div class="panel-heading" role="tab" id="tp">
+                    <h4 class="panel-title">
+                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse-tp" aria-expanded="false" aria-controls="tp">
+                            Title page
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse-tp" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="tp">
+                    <div class="panel-body">
+                        <p>Please see the submission template (HYPERLINK XX).</p>
+                        <p>Every manuscript must contain a title page, which should include:</p>
+                        <ul>
+                            <li>Article title</li>
+                            <li>Author names in order of authorship.</li>
+                            <li>Author information: List of department, institution, city and country for each author. Each unique department should be numbered and included as superscript AFTER the relevant author.</li>
+                            <li>Corresponding author contact details: full name, institution address, email, telephone number of the lead author.</li>
+                            <li>Running title: a short version of the full title (to go at the top of each journal page).</li>
+                            <li>Field of research: basic science: immunology, case study, systematic review etc.</li>
+                            <li>Sources of support: including grants or other support that facilitated completion of the work.</li>
+                            <li>Conflicts of interest</li>
+                            <li>Keywords: 3-5 keywords that best represent the article. These are used for online search engines.</li>
+                            <li>Number of figures: total number of figures (maximum 5).</li>
+                            <li>Number of tables: total number of tables (maximum 3).</li>
+                            <li>Word count, excluding title page, abstract, references, figures and tables.</li>
+                        </ul>
+                    </div>
+                </div>
+
+
+                <div class="panel-heading" role="tab" id="ab">
+                    <h4 class="panel-title">
+                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse-ab" aria-expanded="false" aria-controls="ab">
+                            Abstract
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse-ab" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="ab">
+                    <div class="panel-body">
+                        <p>Please see the submission template (HYPERLINK XX).</p>
+                        <p>The abstract should contain the following:</p>
+                        <ul>
+                            <li>Introduction</li>
+                            <li>Materials and methods</li>
+                            <li>Results</li>
+                            <li>Conclusions</li>
+                        </ul>
+                    </div>
+                </div>
+
+
+                <div class="panel-heading" role="tab" id="in">
+                    <h4 class="panel-title">
+                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse-in" aria-expanded="false" aria-controls="in">
+                            Introduction
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse-in" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="in">
+                    <div class="panel-body">
+                        <p>The introduction should provide context and background for the study. For original research it should contain a hypothesis and aim(s). For audit it should contain a standard(s) and aim(s). Do not include any information from materials and methods, results, discussions, or conclusions in the aims.</p>
+                    </div>
+                </div>
+
+
+                <div class="panel-heading" role="tab" id="mam">
+                    <h4 class="panel-title">
+                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse-mam" aria-expanded="false" aria-controls="mam">
+                            Materials and methods
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse-mam" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="mam">
+                    <div class="panel-body">
+                        <p>The methods section should describe how the study/research was performed. Any materials and/or equipment should be identified followed by manufacturer’s name and city in parentheses. References to established methods should be included, less well-known methods should be described in text, with reasons for using these methods. Generic names should be used for drugs, with dose and route of administration included.</p>
+                    </div>
+                </div>
+
+
+                <div class="panel-heading" role="tab" id="res">
+                    <h4 class="panel-title">
+                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse-res" aria-expanded="false" aria-controls="res">
+                            Results
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse-res" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="res">
+                    <div class="panel-body">
+                        <p>The results section should highlight important findings from the study. Results should be presented in a logical sequence. Do not repeat details found in the methods or introduction. It should include minimal discussion of the results.</p>
+                    </div>
+                </div>
+
+
+                <div class="panel-heading" role="tab" id="dis">
+                    <h4 class="panel-title">
+                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse-dis" aria-expanded="false" aria-controls="dis">
+                            Discussion
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse-dis" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="dis">
+                    <div class="panel-body">
+                        <p>The discussion should emphasise new and important aspects of the study. Authors should discuss the relative importance of the findings in the context of the available literature and the potential for future research or clinical practice.</p>
+                        <p>Authors should avoid repetition of information contained within the results section. However, a brief description of the main findings may be required, followed by potential mechanisms or explanations for these findings.</p>
+                        <p>Authors should avoid over exaggerating the importance of their results, and should not make claims that are not supported up by their results or the literature.</p>
+                    </div>
+                </div>
+
+
+                <div class="panel-heading" role="tab" id="con">
+                    <h4 class="panel-title">
+                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse-con" aria-expanded="false" aria-controls="con">
+                            Conclusions
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse-con" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="con">
+                    <div class="panel-body">
+                        <p>Conclusions should be linked to the aims of the study. Authors should avoid over exaggerated or unsupported conclusions.</p>
+                    </div>
+                </div>
+
+                
+                <div class="panel-heading" role="tab" id="ref">
+                    <h4 class="panel-title">
+                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse-ref" aria-expanded="false" aria-controls="ref">
+                            References
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse-ref" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="ref">
+                    <div class="panel-body">
+                        <p>Please follow the [LINK]style guidelines[/LINK] for references.</p>
+                    </div>
+                </div>
+
+
+                <div class="panel-heading" role="tab" id="rrm">
+                    <h4 class="panel-title">
+                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse-rrm" aria-expanded="false" aria-controls="rrm">
+                            Resubmitting revised manuscripts
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse-rrm" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="rrm">
+                    <div class="panel-body">
+                        <p>Following the peer review process, authors may be asked to re-submit a revised/amended manuscripts. As part of the peer review process authors may receive a document which will suggest changes. ALL suggested changes should be made; authors may make a rebuttal stating why certain suggested changes should not be made. If this is the case the Section Editor, Deputy Editor, or Editor-in-Chief will review the rebuttal and make a final decision on the suggested change in question.</p>
+                        <p>Once the author has made the suggested changes the authors should submit an amended document AND an amended document compared to the original document (so that reviewers can see that the suggested changes have been made). This can be done by selecting Tools-->Track Changes-->Compare Documents-->Then select the original document and the amended document.</p>
+                    </div>
+                </div>                
+            </div>
+        </div>
+
+
+
+        
+    </div>
+</div>
