5ad7f4ba90465724a1031a8fdbaf8b6f3590af1a
Helen Jackson <helen@deckofpandas.co.uk>
2017-02-08 19:50:01 +0000

    Fresh files for NSAMR standalone theme

---
+++ journal-machinery/ojs-3.0.1/plugins/themes/nsamr/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @defgroup plugins_themes_default_child Default theme plugin
+ */
+
+/**
+ * @file plugins/themes/nsamr/index.php
+ *
+ * Copyright (c) 2014-2016 Simon Fraser University Library
+ * Copyright (c) 2003-2016 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_themes_nsamr
+ * @brief Wrapper for default child theme plugin.
+ *
+ */
+
+require_once('NsamrThemePlugin.inc.php');
+
+return new NsamrThemePlugin();
+
+?>
