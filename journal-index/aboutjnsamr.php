88c811d57075fff4f244d43ee7dd76487d4f67e0
Helen Jackson <helen@deckofpandas.co.uk>
2017-03-03 13:07:21 +0000

    New about page with styled accordion

--- journal-index/aboutjnsamr.php
+++ journal-index/aboutjnsamr.php
@@ -20,31 +20,154 @@
 
 <div class="space40"></div> 
 
-<div class="container policies">
+<div class="container">
+    <!-- Top paragraph -->
+    <div class="row">
+        <div class="col-md-6 margin20">
+            <h3 class="heading">A little about us</h3>
+            <p>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni. Dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
+            </p>
+            <p>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni. Dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
+            </p>
+        </div>
+        <div class="col-md-6">
+            <img src="static/images/assorted/manuscript-729617_1920.jpg" class="img-responsive" alt="">
+        </div>
+    </div><!-- End row-->
+
+    <div class="space40"></div> 
+
+    <!-- Start of three columns -->
     <div class="row">
 
-        <!--CONTENT-->
-        <div class="tabs col-sm-9">
-            Words words words
-      </div><!--END CONTENT
-  -->
+        <!-- First column -->
+        <div class="col-md-4 margin20">
+            <h3 class="heading">What do we publish?</h3>
+            <div class="panel-group" id="accordion">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+                                <i class="fa fa-pencil-square-o icon-with-space"></i>    Any original academic material
+                            </a>
+                        </h4>
+                    </div>
+                    <div id="collapseOne" class="panel-collapse collapse in">
+                        <div class="panel-body">
+                            <p>Including articles, abstracts, posters, letters, and case reports, provided that they fall within our remit and that the quality is high enough.</p>
+                            <p>We fully support the publication of negative results.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+                                <i class="fa fa-file-text-o icon-with-space"></i>    Articles
+                            </a>
+                        </h4>
+                    </div>
+                    <div id="collapseTwo" class="panel-collapse collapse">
+                        <div class="panel-body">
+                            Including original experimental research, critical reviews, and lab reports. We are happy to consider publishing the results of very small studies, and even work-in-progress if we consider it to be of interest to the academic community.
+                        </div>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+                                <i class="fa fa-file-image-o icon-with-space"></i>    Abstracts and posters
+                            </a>
+                        </h4>
+                    </div>
+                    <div id="collapseThree" class="panel-collapse collapse">
+                        <div class="panel-body">
+                            We offer a platform for the publication of abstracts and posters (provided they are not already published elsewhere). This is an excellent way for you to disseminate work that you might have done as part of your course. If your abstract describes a poster that you also submit to us, we will review and publish them together as a set.
+                        </div>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour">
+                                <i class="fa fa-envelope-o icon-with-space"></i>    Letters
+                            </a>
+                        </h4>
+                    </div>
+                    <div id="collapseFour" class="panel-collapse collapse">
+                        <div class="panel-body">
+                            We offer the opportunity for you to respond to work that we have previously pubilshed by writing us a letter. Furthermore, we are always interested in the views of medical students regarding current research or topical news.
+                        </div>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseFive">
+                                <i class="fa fa-stethoscope icon-with-space"></i>    Case reports
+                            </a>
+                        </h4>
+                    </div>
+                    <div id="collapseFive" class="panel-collapse collapse">
+                        <div class="panel-body">
+                            We promote the publication of reports covering striking or perhaps unusual cases, as this provides both interest and a wider general medical education.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-      <!--SIDEBAR-->
-      <div class="col-sm-3 margin40">
-        <div class="side-nav-wrapper">
-            <ul class="list-unstyled link-list">
-                <li><a href="#" class="active">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
+        <!-- Second column -->
+        <div class="col-md-4 margin20">
+            <h3 class="heading">What areas do we cover?</h3>
+            <div class="space20"></div>
+            <div class="skills-wrapper wow animated bounceIn animated" data-wow-delay="0.2s">
+                <h4 class="heading-progress">Cardiology <span class="pull-right">88%</span></h3>
+                <div class="progress">
+                    <div class="progress-bar" style="width: 88%" aria-valuemax="100" aria-valuemin="0" aria-valuenow="88" role="progressbar">
+                    </div>
+                </div>
+                <h4 class="heading-progress">Endocrinology <span class="pull-right">78%</span></h3>
+                <div class="progress">
+                    <div class="progress-bar" style="width: 78%" aria-valuemax="100" aria-valuemin="0" aria-valuenow="78" role="progressbar">
+                    </div>
+                </div>
+                <h4 class="heading-progress">Neurology <span class="pull-right">82%</span></h3>
+                <div class="progress">
+                    <div class="progress-bar" style="width: 82%" aria-valuemax="100" aria-valuemin="0" aria-valuenow="82" role="progressbar">
+                    </div>
+                </div>   
+                <h4 class="heading-progress">Psychiatry <span class="pull-right">72%</span></h3>
+                <div class="progress">
+                    <div class="progress-bar" style="width: 72%" aria-valuemax="100" aria-valuemin="0" aria-valuenow="72" role="progressbar">
+                    </div>
+                </div> 
+            </div>
+        </div>
+
+        <!-- Third column -->
+        <div class="col-md-4 margin20">
+            <h3 class="heading">Reasons to publish with us</h3>
+            <div class="space20"></div>
+            <ul class="list-unstyled highlight-list">
+                <li><i class="fa fa-check icon-with-space"></i>The first high-quality Open Access student-led academic journal in the UK</li>
+                <li><i class="fa fa-check icon-with-space"></i>No publication fees. Ever.</li>
+                <li><i class="fa fa-check icon-with-space"></i>Submission-to-publication time of three weeks (depending on number of review rounds)</li>
+                <li><i class="fa fa-check icon-with-space"></i>Wide range of articles and areas</li>
+                <li><i class="fa fa-check icon-with-space"></i>Transparent peer-review process with publication of all correspondence and manuscript versions</li>
+                <li><i class="fa fa-check icon-with-space"></i>Robust and user-friendly submission portal</li>
+                <li><i class="fa fa-check icon-with-space"></i>Excellent Open credentials</li>
+                
             </ul>
         </div>
-    </div><!--END SIDEBAR-->
-</div><!--END ROW-->
-<div class="space40"></div> 
-</div><!--END MAIN BODY CONTAINER-->
+    </div><!-- End row -->
+
+</div><!-- End container-->
+
+</div><!-- End main body-->
 
 <div class="space40"></div> 
 <!---PAGE FOOTER AND SCRIPTS-->
