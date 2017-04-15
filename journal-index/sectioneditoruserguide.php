a95f1fec8950a416869ea1216a769b802ea8c8cc
Stephanie Rees <Stephrees95@gmail.com>
2017-04-15 12:06:50 +0100

    section editor guide added

---
+++ journal-index/sectioneditoruserguide.php
@@ -0,0 +1,109 @@
+<!---STANDARD HEAD INFO AND NAV-->
+<?php include('header.php'); ?>
+
+<!---BODY OF THIS PAGE-->
+
+
+<!--call to action-->
+<div class="pattern-bg-mid-shorter">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8">
+                <div class="left-title-bar">
+                    <h2>Section Editor User Guide</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--end call to action-->
+
+<div class="space40"></div> 
+
+<div class="container policies">
+    <div class="row">
+
+
+
+<!--START Section Editor GUIDE-->
+<div class="row">
+    <p>Congratulations on receiving the appointment as Section Editor. This guide will walk you through what you should be doing to delegate work to peer reviewers. </p>
+    <div class="left-heading">1. Recieveing a manuscript</div>
+    <div>
+        <ol>
+            <li>You should have received an email from the Journal Editors with the title “Article review request”. This means you have already been assigned as the Section Editor in charge of this manuscript.</li>
+            <li>Log into http://www.nsamr.ac.uk/journal/journal-index/index.php*** with your username and password. If you do not already have an account, click sign up.</li>
+            <li>Once logged in, click “Tasks” on left hand tab on screen</li>
+            <li>If you have a manuscript due to be sent out to reviewers, you should have a notification on the “tasks” tab. Sometimes this does not register so regardless whether you have notifications on “tasks” or not, check your “My Assigned” tab and you should see the new manuscript submitted, with the stage as “Submission”. </li>
+            <li>Click on “Submissions”</li>
+            <li>You can download the paper by clicking on it under “submission files”. Read through it and decide firstly, if the paper falls under the scope of the journal and meets its generic requirement. If so, click the blue “send to review” button on the right hand side of the screen</li>
+        </ol>    
+    </div>
+</div>
+<hr>
+
+
+<div class="row">
+    <div class="left-heading">2. Selecting Reviewers</div>
+    <div>
+        <ol>
+            <li>from the excel sheet, select which stream of peer reviewers you wish to draw reviewers from. There are currently 5 streams encompassing the various specialties:</li>
+                <ul>
+                    <li>General</li>
+                    <li>Surgery (Surgery, Orthopaedics, trauma)</li>
+                    <li>Internal medicine (Acute medicine/ emergency, anaesthetics, cardiology, paediatrics/obstetrics and gynaecology, neurology, oncology, psychiatry, radiology)</li>
+                    <li>Basic Sciences (microbiology, genetics, infectious disease, immunology/haematology, endocrinology, pharmacology)</li>
+                    <li>Public health (medical education + public health)</li>
+                </ul>    
+            <li>From the excel sheet, the names will be randomised automatically and you will select the first 5 names. This is to maximise reachability while ensuring fairness and equal distribution of papers to reviewers. </li>
+        </ol>
+    </div>
+</div>  
+<hr>
+
+
+
+<div class="row">
+    <div class="left-heading">3. Allocating Reviewers</div>
+    <div>
+        <ol>
+            <li>You are then required to add these 5 names/email addresses manually on the website by searching their name and if they are not there, you have to create a profile for them by clicking “create new reviewer”  </li>
+            <li>If you are creating a new reviewer, fill in the form with details from the excel sheet. The username you create should be first name and surname. If it is taken add a number after  </li>
+            <li>You then set the response due date as 48 hours and actual review due date as 2 weeks. Remember to attach the manuscript as well.</li>
+            <li>After pressing send, the selected peer reviewers will then receive an email asking them if they wish to accept the task. They will then log in, download the manuscript and undertake the review as per the instructions within the Peer Reviewer Pack given to them.</li>
+        </ol>
+    </div>
+</div>  
+<hr>
+
+<div class="row">
+    <div class="left-heading">4. Getting a completed review</div>
+    <div>
+        <ol>
+            <li>Once they accept the Peer Review request, you as Section Editor will receive an email saying the Peer Reviewer has accepted the task. Upon their completion, you will get an email as well saying that reviews are in and your decision is required.</li>
+            <li>You will then log on using the link provided.</li>
+            <li>You can read through the reviews and make sure that the review aligns with our Peer Reviewer guidelines. If any of the peer reviewers request for revisions, click “Request Revisions” from the right side of the page.</li>
+            <li>Press “Add Reviews to Email” and this will automatically add the Peer Reviewer’s comments for the authors to read. </li>
+            <li>Press “Record Editorial Decision” and the revisions will be sent to the authors to make changes.</li>
+            <li>The authors will send their revisions back and the process repeats itself. Lastly, once reviewers and yourself are happy with the manuscript, Add one of the editorial team as a participant and inform them so they can perform the final review.</li>
+            <li>The editorial team will then send the manuscript for copyediting. </li>
+            <li>Your job is done</li>
+        </ol>
+    </div>
+</div>  
+<hr>
+<!--END AUTHOR GUIDE-->
+
+
+
+
+
+
+
+</div><!--END ROW-->
+<div class="space40"></div> 
+</div><!--END MAIN BODY CONTAINER-->
+
+<div class="space40"></div> 
+<!---PAGE FOOTER AND SCRIPTS-->
+<?php include('footer.php'); ?>
