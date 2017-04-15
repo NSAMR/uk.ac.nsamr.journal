7985b2f9eb55e0da26be70943e2d70091ea11fa2
Stephanie Rees <Stephrees95@gmail.com>
2017-04-15 11:41:29 +0100

    author guide

--- journal-index/authorguide.php
+++ journal-index/authorguide.php
@@ -10,7 +10,7 @@
         <div class="row">
             <div class="col-sm-8">
                 <div class="left-title-bar">
-                    <h2>AKANKSHA PUT YOUR TITLE HERE</h2>
+                    <h2>Author User Guide</h2>
                 </div>
             </div>
         </div>
@@ -27,38 +27,272 @@
 
 <!--START AUTHOR GUIDE-->
 <div class="row">
-    <div class="left-heading">Eligibility</div>
+    <p>Thank you for considering to make a submission with JSAMR. This guide will walk you through the steps of making a submission on the website.</p>
+    <div class="left-heading">1. Register to be an author</div>
     <div>
-        <p>All articles must have a medical student as first author and corresponding author. The medical student must be enrolled at an accredited medical school.</p>
-        <p>The first author must submit the article using their institutional email address (e.g. @ac.uk, @.edu). The editors retain the right to contact the first author's institution to confirm their enrollment.</p>
+        <p>Register on [weblink-journal machinery***] to create an account as an author.</p>
+        <p>SS: registration page (removed would you like to be reviewer box)?ann=affiliation = uni*** </p>
     </div>
 </div>
 <hr>
 
 
 <div class="row">
-    <div class="left-heading">Copyright</div>
+    <div class="left-heading">2. Prepare Submission</div>
     <div>
-        <p>Authors who publish with JNSAMR agree to the following:</p>
-        <ul>
-            <li>We are the authors of the submitted manuscript/submission as described in the JNSAMR ethics policy</li>
-            <li>We have not excluded any individuals who qualify as authors from our submission to JNSAMR</li>
-            <li>We agree to the submission of this work and to its publication, upon acceptance</li>
-            <li>We certify the originality of this work, and that it does not infringe upon any copyright</li>
-            <li>We declare that this work has not been published or is under consideration for publication elsewhere</li>
-            <li>We agree to not submit this article for publication elsewhere until a final decision has been received from JNSAMR</li>
-            <li>We declare that any necessary ethical approval was obtained</li>
-            <li>We declare that any conflicts of interest have been disclosed</li>
-            <li>I take full responsibility for any breach of consent and/or violation of medical legal practices committed</li>
-            <li>I assume all legal responsibility for any infraction regarding information published by JNSAMR on my behalf</li>
-            <li>We hereby transfer, assign, or otherwise convey all copyright ownership of the manuscript/submission in its entirety, including all text, figures, tables, pictures, and supporting documents, including any rights incidental thereto, exclusively to JNSAMR, in the event that the manuscript/submission is published by JNSAMR</li>
-            <li>We agree to immediate open access for any accepted manuscript/submission published by JNSAMR</li>
-        </ul>
+        <p>How to submit your work - [journal index work – timeline weblink]. </p>
+        <p>This page outlines how to prepare your work before a submission to improve the chance of acceptance and publish.</p> 
+        <p>Ready to submit? Click ‘New Submission’</p>
     </div>
 </div>  
 <hr>
 
+<div class="row">
+    <div class="left-heading">3. Submit Manuscript</div>
+    <div>
+        <ol>
+        <li>Section: Select either ‘Article’ or ‘Letter’ from the drop-down menu</li>
+        <li>Submissioon requirements: All the requirements should be ticked</li>
+        <li>Comments for the Editor: Choose the most suitable 2-3 codes for your article/letter from the following:</li> 
+        <div class="container">            
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Code</th>
+                <th>Speciality</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>100</td>
+                <td>Anaesthesia / Intensive Care medicine</td>
+              </tr>
+              <tr>
+                <td>200</td>
+                <td>Cardiology / Cardiovascular medicine</td>
+              </tr>
+              <tr>
+                <td>300</td>
+                <td>Cardiothoracic Surgery</td>
+              </tr>
+              <tr>
+                <td>400</td>
+                <td>CLinical Genetics</td>
+              </tr>
+              <tr>
+                <td>500</td>
+                <td>Clinical immunology</td>
+              </tr>
+              <tr>
+                <td>600</td>
+                <td>Clinical pathology</td>
+              </tr>
+              <tr>
+                <td>700</td>
+                <td>Clinical pharmacology and therapeutics</td>
+              </tr>
+              <tr>
+                <td>800</td>
+                <td>Dermatology</td>
+              </tr>
+              <tr>
+                <td>900</td>
+                <td>Ear, nose and throat / Otolaryngology</td>
+              </tr>
+              <tr>
+                <td>1000</td>
+                <td>Emergency / Acute medicine</td>
+              </tr>
+              <tr>
+                <td>1100</td>
+                <td>Endocrinology and Diabetes</td>
+               </tr> 
+               <tr>
+                    <td>1200</td>
+                    <td>Gastroenterology</td>
+                </tr>
+                <tr>
+                    <td>1300</td>
+                    <td>General internal medicine</td>
+                </tr>
+                <tr >   
+                    <td>1400</td>
+                    <td>General Practice / Primary Care</td>
+                </tr>    
+                <tr>
+                    <td>1500</td>
+                    <td>General surgery</td>
+                </tr>
+                <tr>
+                    <td>1600</td>
+                    <td>Genito-urinary medicine</td>
+                </tr>
+                <tr>
+                    <td>1700</td>
+                    <td>Haematology</td>
+                </tr>
+                <tr>
+                    <td>1800</td>
+                    <td>Hepatobiliary medicine</td>
+                </tr>
+                <tr>
+                    <td>1900</td>
+                    <td>Histopathology</td>
+                </tr>
+                <tr>
+                    <td>2000</td>
+                    <td>Immunology</td>
+                </tr>
+                <tr>
+                    <td>2100</td>
+                    <td>Infectious diseases</td>
+                </tr>
+                <tr>
+                    <td>2200</td>
+                    <td>Medical education</td>
+                </tr>
+                <tr>
+                    <td>2300</td>
+                    <td>Medicine for the Elderly</td>
+                </tr>
 
+                <tr>
+                    <td>2400</td>
+                    <td>Neurology / Neuroscience</td>
+                </tr>
+                <tr>
+                    <td>2500</td>
+                    <td>Neurosurgery</td>
+                </tr>
+                <tr>
+                    <td>2600</td>
+                    <td>Obstetrics and Gynaecology</td>
+                </tr>
+                <tr>
+                    <td>2700</td>
+                    <td>Oncology</td>
+                </tr>
+                <tr>
+                    <td>2800</td>
+                    <td>Ophthalmology</td>
+                </tr>
+                <tr>
+                    <td>2900</td>
+                    <td>Oral and Maxillofacial surgery</td>
+                </tr>
+                <tr>
+                    <td>3000</td>
+                    <td>Palliative medicine</td>
+                </tr>
+                <tr>
+                    <td>3100</td>
+                    <td>Paediatrics and Child Health</td>
+                </tr>
+                <tr>
+                    <td>3200</td>
+                    <td>Plastic surgery</td>
+                </tr>
+                <tr>
+                    <td>3300</td>
+                    <td>Psychiatry / Mental Health</td>
+                </tr>
+                <tr>
+                    <td>3400</td>
+                    <td>Public Health</td>
+                </tr>
+                <tr>
+                    <td>3500</td>
+                    <td>Radiology / Nuclear medicine</td>
+                </tr>
+                <tr>
+                    <td>3600</td>
+                    <td>Renal medicine</td>
+                </tr>
+                <tr>
+                    <td>3700</td>
+                    <td>Respiratory medicine</td>
+                </tr>
+                <tr>
+                    <td>3800</td>
+                    <td>Rheumatology</td>
+                </tr>
+                <tr>
+                    <td>3900</td>
+                    <td>Trauma / Orthopaedics</td>
+                </tr>
+                <tr>
+                   <td>4000</td>
+                   <td>Urology</td>
+                </tr>
+            </tbody>
+          </table>
+        </div>
+        <li>Click 'Save and Continue'</li>
+        </ol>
+    </div>
+</div>  
+<hr>
+
+<div class="row">
+    <div class="left-heading">4. Upload FIle</div>
+    <div>
+        <ol>
+        <li>Upload the respective file after you select the component of your work that you wish to upload</li>
+        <p>[This will change once we decide on article components***]</p>
+        <li>Review Details - Ensure the correct file has been uploaded for each component.</li>
+        <li>Confirm - Click ‘Add another file’ and repeat the upload submission process until all relevant the ‘Article Components’ of your work are uploaded. Then, click ‘Complete’.</li>
+        <li>Click ‘Save and continue’</li>
+        <li>Enter the following metadata
+            <ul>
+            <li>Title*</li>
+            <li>Prefix*</li>
+            <li>Abstract*: A good abstract is generally structured and 200-300 words.</li>
+            <li>List of contributors: Add all the contributing authors of the work being submitted.</li>  
+            </ul>  
+        </li>
+        <li>Tick ‘Principal contact for editorial correspondence’ for the appropriate contributor.</li>
+        <li>‘Include this contributor in browse lists?’: JSAMR allows the website users to see all articles that are attributed to a particular author; if the author wishes to be browsed in this manner, tick this option.</li>
+        <li>Click ‘Save and continue’</li>
+        </ol>
+    </div>
+</div>  
+<hr>
+
+<div class="row">
+    <div class="left-heading">5. Finalising Submission</div>
+    <div>
+        <p>Confirmation - Once you have reviewed all sections and are confident to make the submission, click ‘Finish Submission’</p>
+    </div>
+</div>  
+<hr>
+
+<div class="row">
+    <div class="left-heading">6. Track your Submission</div>
+    <div>
+        <p>Once your submission has undergone a cycle of peer review and been assigned a decision, you will receive an email notification to log in and check the website.</p>
+        <p> Click on ‘Editor Decision’ to view the outcome which can be one of the following:
+            <ul>
+                <li>Accept submission</li>
+                <li>Revisions required</li>
+                <li>Resubmit for review</li>
+                <li>Resubmit elsewhere</li>
+                <li>Decline submission</li>
+            </ul>    
+        <p>Generally, you will be able to see the decision and the comments received by (anonymized) peer reviewers. Then, you will take the appropriate action required.</p>
+        <p>  If you need to resubmit your work after making revisions, upload the revised document (‘Upload File’) under Revisions.</p>
+        <p> If you want to make comments/get advice on something, use the ‘Add discussion’ button under Review Discussions.</p>
+
+    </div>
+</div>  
+<hr>
+
+<div class="row">
+    <div class="left-heading">7. Copy Editing</div>
+    <div>
+        <p>Once your manuscript has been accepted it will then be sent to copy editing, which checks for grammatical errors. You will then be emailed a final draft for you to check over and approve. Once this draft has been approved it will be published</p>
+    </div>
+</div>  
+<hr>
 <!--END AUTHOR GUIDE-->
 
 
