347ba22f69a3a33ca800fb8d9ce0e4aa398891de
Stephanie Rees <Stephrees95@gmail.com>
2017-04-16 16:18:22 +0100

    working out #s

--- journal-index/checklist.php
+++ journal-index/checklist.php
@@ -1,5 +1,39 @@
+<!---STANDARD HEAD INFO AND NAV-->
+<?php include('header.php'); ?>
 
-        <div class="left-heading"></i>Checklist</div>
+<!---BODY OF THIS PAGE-->
+
+
+<!--call to action-->
+<div class="pattern-bg-mid-shorter">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8">
+                <div class="left-title-bar">
+                    <h2>SUbmission Checklist</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--end call to action-->
+
+<!-- Breadcrumbs -->
+<div class="pattern-bg-light-supershort col-sm-12 hidden-xs text-right">
+    <div class="container">
+        <div class="row">
+            <ol class="breadcrumb">
+                <li><a href="index.html">Home</a></li>
+                <li>Checklist</li>
+            </ol>
+        </div>
+    </div>
+</div><!-- End breadcrumbs-->
+<div class="space40"></div>
+<div class="space40"></div> <!-- TODO Why did I have to put in two of these...? -->
+
+<div class="container">
+    <div class="row">   
         <p>To increase the chance of your submission being accepted please check the following:</p>
 
         <div class="left-subheading">Author Information</div>
@@ -64,3 +98,8 @@
         </ul>                                                                                                        
     </div>
 </div>
+
+
+<div class="space40"></div> 
+<!---PAGE FOOTER AND SCRIPTS-->
+<?php include('footer.php'); ?>
