2f6d31f21c5a0e158ac844295b7718c4b45af736
Stephanie Rees <Stephrees95@gmail.com>
2017-04-16 11:30:18 +0100

    breadcrumbs t&c, privacy policy

--- journal-index/privacypolicy.php
+++ journal-index/privacypolicy.php
@@ -19,6 +19,18 @@
 </div>
 <!--end call to action-->
 
+<!-- Breadcrumbs -->
+<div class="pattern-bg-light-supershort col-sm-12 hidden-xs text-right">
+    <div class="container">
+        <div class="row">
+            <ol class="breadcrumb">
+                <li><a href="index.html">Home</a></li>
+                <li>Privacy Policy</li>
+            </ol>
+        </div>
+    </div>
+</div><!-- End breadcrumbs-->
+<div class="space40"></div>
 <div class="space40"></div> 
 
 <div class="container policies">
