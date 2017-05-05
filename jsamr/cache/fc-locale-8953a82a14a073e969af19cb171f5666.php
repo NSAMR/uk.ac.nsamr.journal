419d880054c0d04eab03ea46942412ef03389388
Helen Jackson <helen@deckofpandas.co.uk>
2017-05-05 10:46:13 +0100

    Remove cache files (again)

--- jsamr/cache/fc-locale-8953a82a14a073e969af19cb171f5666.php
+++
@@ -1,7 +0,0 @@
-<?php return array (
-  'plugins.block.relatedItems.displayName' => 'Related Items Block',
-  'plugins.block.relatedItems.description' => 'This plugin displays a block containing related items in the reading tools sidebar.',
-  'plugins.block.relatedItems.title' => 'Related Items',
-  'plugins.block.relatedItems.hide' => 'Hide',
-  'plugins.block.relatedItems.show' => 'Show all',
-); ?>
